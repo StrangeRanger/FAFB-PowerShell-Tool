0a55f8a607954c27cbf2f68342a548143fdfa26c
Hunter T <strangeranger@hthompson.dev>
2023-11-16 13:06:58 -0800

    Add Test and Package projects

---
+++ FAFB-PowerShell-Tool.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FAFB_PowerShell_Tool.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
