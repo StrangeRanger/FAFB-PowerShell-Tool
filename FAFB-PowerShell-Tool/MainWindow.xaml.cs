202d662f474b3ffbdb4bb5f7e3a9125b155f87d9
Hunter T <strangeranger@hthompson.dev>
2023-11-20 14:37:30 -0800

    Update method and GUI property names

--- FAFB-PowerShell-Tool/MainWindow.xaml.cs
+++ FAFB-PowerShell-Tool/MainWindow.xaml.cs
@@ -14,27 +14,27 @@ namespace FAFB_PowerShell_Tool
             InitializeComponent();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void CommandButton1(object sender, RoutedEventArgs e)
         {
             command = "Get-Process";
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void CommandButton2(object sender, RoutedEventArgs e)
         {
             command = "Get-Process | Out-String -Width 4096";
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void CommandButton3(object sender, RoutedEventArgs e)
         {
             command = "Get-ChildItem -Path $env:USERPROFILE | Out-String -Width 4096";
         }
 
-        private void Button_Click_4(object sender, RoutedEventArgs e)
+        private void CommandButton4(object sender, RoutedEventArgs e)
         {
             command = "New-Item -Path $env:USERPROFILE\\myFile.txt -ItemType File";
         }
 
-        private void Button_Click_5(object sender, RoutedEventArgs e)
+        private void ExecutionButton(object sender, RoutedEventArgs e)
         {
             List<string> commandOutput = PowerShellExecutor.Execute(command);
             string fullCommandOutput = "";
