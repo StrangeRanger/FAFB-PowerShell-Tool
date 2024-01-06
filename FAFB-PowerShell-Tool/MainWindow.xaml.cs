9da30ec8f60f9ebba23838073f92cffec94af105
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2024-01-06 11:17:33 -0800

    Working on serialiazation to work

--- FAFB-PowerShell-Tool/MainWindow.xaml.cs
+++ FAFB-PowerShell-Tool/MainWindow.xaml.cs
@@ -1,14 +1,24 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Management.Automation;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using FAFB_PowerShell_Tool.PowerShell;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Net.Security;
+
 
 namespace FAFB_PowerShell_Tool;
 
 public partial class MainWindow
 {
+    ObservableCollection<Command> saved_commands = new ObservableCollection<Command>();
+    List<Button> custom_buttons = new List<Button>();
     public MainWindow()
     {
         InitializeComponent();
@@ -21,6 +31,9 @@ public partial class MainWindow
         ObservableCollection<Command> list = await ActiveDirectoryCommands.GetActiveDirectoryCommands();
         comboBoxCommandList.ItemsSource = list;
         comboBoxCommandList.DisplayMemberPath = "CommandName";
+
+
+        FillCustomQueries();
     }
     
       /* NOTE: Temporary method for testing purposes only.
@@ -85,6 +98,7 @@ public partial class MainWindow
     }
 
     // TODO: Test to see if this works as it should...
+    // TODO: Make the buttons able to save a command to them and have a universal class maybe for executing them
     private void MSaveQueryButton(object sender, RoutedEventArgs e)
     {
         // Try to get the content within the drop downs
@@ -96,15 +110,37 @@ public partial class MainWindow
 
             Button newButton = new()
             {
+               
                 Content = "Special Command",
                 Height = 48
             };
 
+            CustomQueries.CustomQueryButtons.Add(newButton);
             ButtonStackPanel.Children.Add(newButton);
+
+            //Stream stream = File.Open("CustomQueries.dat", FileMode.Create);
+            CustomQueries.SerializeMethod();
+            
         }
         catch (Exception ex)
         {
             Console.Write(ex);
+        }
+    }
+    /// <summary>
+    /// This Method is to fill the left had side query bar
+    /// </summary>
+    private void FillCustomQueries() {
+        try
+        {
+            CustomQueries.LoadData();
+
+            foreach (Button btn in CustomQueries.CustomQueryButtons)
+            {
+                ButtonStackPanel.Children.Add(btn);
+            }
+        }catch (Exception ex){ 
+            Trace.WriteLine(ex);
         }
     }
 
---
+++ FAFB-PowerShell-Tool/PowerShell/CustomQueries.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FAFB_PowerShell_Tool.PowerShell
+{
+    [Serializable()]
+    internal class CustomQueries : ISerializable
+    {
+        public static List<Button> CustomQueryButtons = new List<Button>();
+        
+        private static readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("CustomQueryButtons", CustomQueryButtons);
+        }
+
+        public CustomQueries(SerializationInfo info, StreamingContext context) {
+            CustomQueryButtons = (List<Button>)info.GetValue("CustomQueryButtons", typeof(List<Button>));
+        }
+
+        public static void SerializeMethod()
+        {
+
+            Stream stream = File.Open("CustomQueries.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, CustomQueryButtons);
+            stream.Close();
+
+            //return JsonSerializer.Serialize(CustomQueryButtons, _options);
+        }
+
+        public static void LoadData() {
+
+            Stream stream = File.Open("CustomQueries.dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            CustomQueryButtons = (List<Button>)bf.Deserialize(stream);
+            stream.Close();
+        }
+    }
+}
