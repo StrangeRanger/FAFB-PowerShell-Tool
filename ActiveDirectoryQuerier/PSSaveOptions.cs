8afc0949cae4047f96cc47eb28b86d0d9bacc2a2
Hunter T <strangeranger@hthompson.dev>
2024-02-27 14:16:22 -0800

    All unused or unimplemented methods now throw NotImplementedException()

--- ActiveDirectoryQuerier/PSSaveOptions.cs
+++ ActiveDirectoryQuerier/PSSaveOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Management.Automation.Internal;
+using System.Management.Automation.Runspaces;
 using ActiveDirectoryQuerier.PowerShell;
 
 namespace ActiveDirectoryQuerier;
@@ -13,43 +14,32 @@ public class PSSaveOptions
     /// This would be to output the query to a csv, will need to eventually adapt to a guicommand as well most likely
     /// </summary>
     /// <param name="commandString"> This is the internal command that we want to output to a csv </param>
-    /*public Command OutputToCSV(Command commandString)
+    public Command OutputToCsv(Command commandString)
     {
-        //Checks to see if the parameters are null,
-        if (commandString.Parameters == null || commandString.Parameters.Count == 0)
-        {
-            //if they are null then make a new array with the output option, currently to relative path to SavedOutput
-    folder commandString.Parameters.Add("| Export-CSV ..\\..\\..\\SavedOutput\\output.csv");
-        }
-        else
-        {
-            // If it is not null we will clone the parameters and add the output parameter to the new string and then
-    set the array string[] temp = commandString.Parameters; Array.Resize(ref temp, temp.Length + 1); temp[temp.Length -
-    1] = "| Export-CSV ..\\..\\..\\SavedOutput\\output.csv";
-            //setting
-            commandString.Parameters = temp;
-        }
-        //return the commandString with the updated parameters
-        return commandString;
-    }*/
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// This would be to have the query output to a message box
     /// </summary>
     public InternalCommand OutputToMessageBox(InternalCommand commandSting)
     {
-        return null;
+        throw new NotImplementedException();
     }
 
     /// <summary>
     /// This will be to save the query to a text file
     /// </summary>
     public void SaveToTxt(ReturnValues results)
-    { }
+    {
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// This is used for saving the file to a PS1 powershell file
     /// </summary>
     public void SaveToPS(ReturnValues results)
-    { }
+    {
+        throw new NotImplementedException();
+    }
 }
