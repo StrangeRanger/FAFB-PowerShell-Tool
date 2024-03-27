eede5e26ea1658b7e4d215ff485df7bc4192418f
Hunter T <strangeranger@hthompson.dev>
2024-03-27 15:48:47 -0700

    New test

---
+++ ActiveDirectoryQuerier.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using ActiveDirectoryQuerier.MessageBoxService;
+using Moq;
+
+namespace ActiveDirectoryQuerier.Tests;
+
+public class MainWindowViewModelTests
+{
+    [Fact]
+    public void EditQueryFromQueryStackPanel_NullParameter_ShowsMessageBox()
+    {
+        // Arrange
+        var mockMessageBoxService = new Mock<IMessageBoxService>();
+        var viewModel = new MainWindowViewModel
+        {
+            MessageBoxService = mockMessageBoxService.Object
+        };
+
+        // Act
+        viewModel.EditQueryFromQueryStackPanel(null);
+
+        // Assert
+        mockMessageBoxService.Verify(
+            x => x.Show(It.IsAny<string>(), "Error", MessageBoxButton.OK, MessageBoxImage.Error),
+            Times.Once);
+    }
+}
